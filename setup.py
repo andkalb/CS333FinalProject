633d3f6a97520554e41c11d4f3676275dfce8122
andkalb <89561907+andkalb@users.noreply.github.com>
2024-05-06 13:08:46 -0700

    Delete setup.py

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(name='TruthTable',
-      version='1.0',
-      description='CS333 Final Project TruthTable Utility',
-      author='Andrew',
-      author_email='',
-      packages=['TruthTable'],
-     )
